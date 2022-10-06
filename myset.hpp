b438fc3c107e36301a518bdb97e134eb7e115183
Mason Acevedo <macevedo@hmc.edu>
2022-10-06 10:43:07 -0700

    More work on header file
    Still not complete, but refreshing myself on stuff!

--- myset.hpp
+++ myset.hpp
@@ -8,25 +8,34 @@
 
 #include <string>
 
-class myset {
+class MySet {
     private:
         class Iterator;
     public:
         MySet();
         ~MySet();
-        void insert(std::string key, int value);
+        void insert(std::string key);
         bool exists(std::string key) const;
         int size() const;
 
         // const keyword after function declaration
         // denotes that calling the function 
-        bool operator==(const mySet rhs) const;
-        bool operator!=() const;
+        bool operator==(const MySet rhs) const;
+        bool operator!=(const MySet rhs) const;
 
-
+        // i think this line enables us to use the 
+        // standard c++ iterator syntax with MySet
         using iterator = Iterator;        
         iterator begin() const; // returns iterator at first non-empty spot in corresponding array
         iterator end() const; // returns invalid/past the end of the tree iterator
-    
+
+        std::ostream& print(std::ostream& stream) const;
+        std::ostream& showStatistics(std::ostream& stream) const;
+
     private: 
-}
+        size_t size;
+        
+
+};
+
+#endif  // MYSET_HPP_INCLUDED
