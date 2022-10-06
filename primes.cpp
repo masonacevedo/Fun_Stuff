aabc281383ba29c5532a04bb4013b41c55284277
Mason Acevedo <macevedo@hmc.edu>
2022-10-05 17:31:44 -0700

    Added a generatePrimes function
    Added a function that returns all the primes less than some integer n. Uses the "sieve" technique.

--- primes.cpp
+++ primes.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 bool divisible(int a, int b){
-    return floor(float(a)/float(b)) == (float(a)/float(b));
+    return floor(double(a)/double(b)) == (double(a)/double(b));
+}
+
+bool generatePrimesHelper (int n, std::vector <int> primes){
+    if (n == 2){
+        return true;
+    }
+    int index = 0;
+    
+    while (primes[index] <= sqrt(n)) {
+        if (divisible(n, primes[index])){
+            return false;
+        }
+        index++;
+    }
+    return true;
+}
+
+std::vector <int> generatePrimes (int n){
+    std::vector <int> primes;
+
+    for (int index = 2; index <= n; index++){
+        if (generatePrimesHelper(index, primes)){
+            primes.push_back(index);
+        }
+    }
+    return primes;
 }
 
 bool isPrime(int n){
-    for (int index = 2; index < sqrt(n); index++){
+    for (int index = 2; index <= sqrt(n); index++){
         if (divisible(n, index)){
             return false;
         }
@@ -16,15 +43,23 @@ bool isPrime(int n){
 
 
 int main(){
+
     int n;
-    std::cout << "Input a number, and I will tell you if it's prime:";
+    std::cout << "Input a number and I give you the primes less than or equal to that number: ";
     std::cin >> n;
     std::cout << "\n";
-    if (isPrime(n)){
-        std::cout << n << " is prime!";
+
+    std::vector primes = generatePrimes(n);
+    std::cout << "primes: [";
+    for (int i = 0; i < primes.size(); i++){
+        if (i == (primes.size()-1)){
+            std::cout << primes[i];
+        }
+        else{
+            std::cout << primes[i] << ", ";
+        }
+        
     }
-    else{
-        std::cout << n << " is composite!";
-    }
-    std::cout << std::endl;
+    std::cout << "]" << std::endl;
+
 }
